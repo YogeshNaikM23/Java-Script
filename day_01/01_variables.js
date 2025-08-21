f57640a1dc540f3434abed777561027757548e9a
YogeshNaikM23 <yogeshnaikm.yn@gmail.com>
2025-08-21 16:38:49 +0000

    dayone completed

---
+++ day_01/01_variables.js
@@ -0,0 +1,24 @@
+const productId=123;
+let nameCustomer="arloh moteo"
+var productName="98uy65"
+
+varName="hello"
+
+//productId=124; //not allowed to modify the const 
+// console.log(productId);
+
+console.table([productId,nameCustomer,productName,varName,])
+
+// {
+//     var name="hello"
+//     console.log(name)
+// }
+
+// console.log(name)
+
+//prefer not to use the var to initilize the variable
+//do use the let and const variables
+
+let accountNumber;
+
+console.log(accountNumber);
---
+++ day_01/03_conversion.js
@@ -0,0 +1,18 @@
+let score=45
+
+console.log("type is : ",typeof(score))
+console.log(score)
+
+let scoreOne="45"
+console.log(typeof(scoreOne))
+console.log(scoreOne)
+let valueIs=Number(scoreOne)
+console.log(typeof(valueIs))
+console.log(valueIs)
+
+let scoreTwo="43abc"
+console.log(typeof(scoreTwo));
+console.log(scoreTwo)
+let valueTwoIs=Number(scoreTwo)
+console.log(typeof(valueTwoIs))
+console.log(valueTwoIs)
