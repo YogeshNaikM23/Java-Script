b03f7bf24de4b6f2e4e65acdbe9056c08a7b9e28
YogeshNaikM23 <yogeshnaikm.yn@gmail.com>
2025-08-23 17:16:04 +0000

    commit me

---
+++ day_03/03_arrow.js
@@ -0,0 +1,29 @@
+// const user={
+//     username:"Monica Gustimbade",
+//     series:"Money Heist",
+//     act:"Robber",
+
+//     welcomeMessage:function(){
+//         console.log(`${this.username}, you are successfully robbed the bank`);
+//         console.log(this)
+        
+//         }
+//     }
+//     user.username="Professor"
+//     user.welcomeMessage()
+//     console.log(this);
+// let  user =(a,b)=>{
+//     console.log(a+b)
+// }
+
+// user(9,4)
+(function chai(){
+    console.log("helo");
+    
+})();
+
+((name)=>{
+    console.log(`hello ${name}`);
+    
+})("yogi")
+
---
+++ day_04/01_iteration.js
@@ -0,0 +1,28 @@
+// let arr=[1,2,3,4,5,[1,2,3]]
+
+// let sValue="hello This is Yogi"
+
+// for (const i of sValue) {
+//     console.log(`hello this characte is : ${i}`)
+// }
+
+// const map = new Map();
+// map.set("IN","India")
+// map.set("USA","United States Of America")
+// map.set("FR","France")
+// map.set("FR","France")
+// console.log(map)
+
+// for (const [i,j] of map){
+//     console.log(i,"-",j)
+// }
+
+const myObject={
+    "name":"Yogi",
+    "age":'32',
+    "year":"final"
+}
+
+for( const [i] of myObject){
+    console.log(i)
+}
